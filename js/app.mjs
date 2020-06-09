a088906df4c6acfbda8fad65cf18e46ee1fbcd0e
domuk-k <dannyworks102@gmail.com>
2020-06-09 16:40:53 +0900

    fix: merged files resolve

--- js/app.mjs
+++ js/app.mjs
@@ -1,27 +1,17 @@
-<<<<<<< HEAD
 import { initBackground } from "./src/background.mjs";
 import { initWeather } from "./src/weather.mjs";
 import { renderClock as initClock } from "./src/clock.mjs";
-import { getQuote as initQuote }from "./src/quote.mjs";
+import { getQuote as initQuote } from "./src/quote.mjs";
+import { init as greeting } from "./src/greeting.mjs";
+import { init as mainTodo } from "./src/mainTodo.mjs";
+import { init as subTodos } from "./src/subTodos.mjs";
 
 window.onload = () => {
   initClock();
   initQuote();
-  initBackground();
   initWeather();
+  initBackground();
+  greeting();
+  mainTodo();
+  subTodos();
 };
-=======
-import { init as greeting } from './src/greeting.mjs'
-import { init as mainTodo } from './src/mainTodo.mjs'
-import { init as subTodos } from './src/subTodos.mjs'
-import { initWeather } from './src/weather.mjs'
-import { initBackground } from './src/background.mjs'
-
-window.onload = () => {
-  initWeather()
-  initBackground()
-  greeting()
-  mainTodo()
-  subTodos()
-}
->>>>>>> 32392062770d9d1f7c7d2eed4d2bb9694ff5acd4
--- js/src/weather.mjs
+++ js/src/weather.mjs
@@ -1,33 +1,33 @@
 // state
-let state = JSON.parse(localStorage.getItem('weatherState'))
+let state = JSON.parse(localStorage.getItem("weatherState"));
 
 // utils
-const API_KEY = '2a12527f777b1cbb783fcf5604ac51ae'
-const needForecast = true ? '' : 'daily'
-const unitSetting = true ? 'metric' : 'imperial'
+const API_KEY = "2a12527f777b1cbb783fcf5604ac51ae";
+const needForecast = true ? "" : "daily";
+const unitSetting = true ? "metric" : "imperial";
 const icons = {
-  'clear sky': 'B',
-  'few clouds': 'H',
-  'overcast clouds': 'Y',
-  'scattered clouds': 'H',
-  'broken clouds': 'H',
-  'shower rain': 'T',
-  'heavy intensity rain': 'R',
-  rain: 'Q',
-  thunderstorm: '0',
-  snow: 'U',
-  mist: 'M',
-  'moderate rain': 'R',
-  'light rain': 'R'
+  "clear sky": "B",
+  "few clouds": "H",
+  "overcast clouds": "Y",
+  "scattered clouds": "H",
+  "broken clouds": "H",
+  "shower rain": "T",
+  "heavy intensity rain": "R",
+  rain: "Q",
+  thunderstorm: "0",
+  snow: "U",
+  mist: "M",
+  "moderate rain": "R",
+  "light rain": "R",
   // "C":'*',
   // "F":'+',
-}
+};
 // DOMs
-const $layoutWeather = document.getElementById('layout-weather')
+const $layoutWeather = document.getElementById("layout-weather");
 
 // templates
 const Template = (function () {
-  function Template () {}
+  function Template() {}
   Template.prototype.init = () => {
     $layoutWeather.innerHTML = `
     <section class="current">
@@ -40,23 +40,24 @@ const Template = (function () {
         </ul>
       </section>
     </section>
-    `
-  }
+    `;
+  };
   Template.prototype.current = () => {
-    document.querySelector('.current').innerHTML = `
+    document.querySelector(".current").innerHTML = `
             <div class="summary">
               <span class="icon">${icons[state.current.description]}</span>
               <span class="temp">${Math.round(state.current.temp)}°</span>
             </div>
             <div class="location">${state.timezone}</div>
-          `
-  }
+          `;
+  };
   Template.prototype.selected = () => {
-    console.log(state)
-    document.querySelector('.selected').innerHTML = `
+    document.querySelector(".selected").innerHTML = `
             <div class="selected-location">${state.timezone}</div>
-            <div class="selected-status">${state.days[0].weather[0].description[0].toUpperCase() +
-              state.days[0].weather[0].description.slice(1)}</div>
+            <div class="selected-status">${
+              state.days[0].weather[0].description[0].toUpperCase() +
+              state.days[0].weather[0].description.slice(1)
+            }</div>
             <div class="selected-summary">
               <span class="selected-icon">${
                 icons[state.days[0].weather[0].description]
@@ -69,12 +70,12 @@ const Template = (function () {
               )}°</span>
             </div>
           
-          `
-  }
+          `;
+  };
   Template.prototype.forecast = () => {
-    let listItems = ''
+    let listItems = "";
 
-    state.days.forEach(day => {
+    state.days.forEach((day) => {
       listItems += `
                     <li class="forecast-item">
                       <div class="item-name">${new Date(day.dt * 1000)
@@ -91,69 +92,64 @@ const Template = (function () {
                         day.temp.min
                       )}°</span>
                     </li>
-                  `
-    })
-    document.querySelector('.forecast-list').innerHTML = listItems
-  }
+                  `;
+    });
+    document.querySelector(".forecast-list").innerHTML = listItems;
+  };
+  return Template;
+})();
 
-  //   <section class="selected-and-forecast">
-
-  // </section>
-
-  return Template
-})()
-
-const template = new Template()
+const template = new Template();
 
 // functions
 const render = () => {
-  state = JSON.parse(localStorage.getItem('weatherState'))
-  template.init()
-  template.current()
-  template.selected()
-  template.forecast()
-}
+  state = JSON.parse(localStorage.getItem("weatherState"));
+  template.init();
+  template.current();
+  template.selected();
+  template.forecast();
+};
 
 const setState = (current, daily, timezone) => {
   localStorage.setItem(
-    'weatherState',
+    "weatherState",
     JSON.stringify({
       current: {
         dt: current.dt,
         temp: current.temp,
         description: current.weather[0].description,
-        mainly: current.weather[0].main
+        mainly: current.weather[0].main,
       },
       days: daily.slice(0, 5),
-      timezone
+      timezone,
     })
-  )
-}
+  );
+};
 
 const getData = async () => {
-  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${37.5326}&lon=${127.024612}&exclude=hourly${needForecast}&appid=${API_KEY}&units=${unitSetting}`
+  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${37.5326}&lon=${127.024612}&exclude=hourly${needForecast}&appid=${API_KEY}&units=${unitSetting}`;
 
-  const response = await fetch(URL).catch(console.log)
-  const data = await response.json()
-  const { current, daily, timezone } = data
+  const response = await fetch(URL).catch(console.log);
+  const data = await response.json();
+  const { current, daily, timezone } = data;
 
-  setState(current, daily, timezone)
-}
+  setState(current, daily, timezone);
+};
 
 const initWeather = async () => {
-  if (!state) await getData()
-  render()
-}
+  if (!state) await getData();
+  render();
+};
 
 $layoutWeather.onclick = ({ target }) => {
-  if (!target.matches('.location')) return
-  const $modal = document.querySelector('.selected-forecast')
-  $modal.classList.toggle('weather-active')
-}
+  if (!target.matches(".location")) return;
+  const $modal = document.querySelector(".selected-forecast");
+  $modal.classList.toggle("weather-active");
+};
 
 $layoutWeather.onclick = ({ target }) => {
-  if (!target.matches('.forecast-item')) return
-  console.log(target)
-}
+  if (!target.matches(".forecast-item")) return;
+  console.log(target);
+};
 
-export { initWeather }
+export { initWeather };
