9a356c49911324c986452cbce898e098384a33d0
domuk-k <dannyworks102@gmail.com>
2020-06-09 15:59:47 +0900

    feat: weather styling & modal

--- js/src/weather.mjs
+++ js/src/weather.mjs
@@ -9,8 +9,8 @@ const icons = {
   "clear sky": "B",
   "few clouds": "H",
   "overcast clouds": "Y",
-  "scattered clouds": "N",
-  "broken clouds": "Y",
+  "scattered clouds": "H",
+  "broken clouds": "H",
   "shower rain": "T",
   "heavy intensity rain": "R",
   rain: "Q",
@@ -18,50 +18,84 @@ const icons = {
   snow: "U",
   mist: "M",
   "moderate rain": "R",
+  "light rain": "R",
   // "C":'*',
   // "F":'+',
 };
 // DOMs
 const $layoutWeather = document.getElementById("layout-weather");
-const $selectedAndForecast = document.createElement("selected-and-forecast");
 
 // templates
 const Template = (function () {
   function Template() {}
-
-  Template.prototype.current = () => {
+  Template.prototype.init = () => {
     $layoutWeather.innerHTML = `
-          <section class="current">
+    <section class="current">
+    </section>
+    <section class="selected-forecast">
+      <section class="selected">
+      </section>
+      <section class="forecast">
+        <ul class="forecast-list">
+        </ul>
+      </section>
+    </section>
+    `;
+  };
+  Template.prototype.current = () => {
+    document.querySelector(".current").innerHTML = `
             <div class="summary">
-              <div class="icon">${icons[state.current.description]}</div>
-              <div class="stats">${state.current.temp}°</div>
+              <span class="icon">${icons[state.current.description]}</span>
+              <span class="temp">${Math.round(state.current.temp)}°</span>
             </div>
             <div class="location">${state.timezone}</div>
-          </section>`;
+          `;
   };
-  Template.prototype.selected = (loc, status, icon, stats) => {
-    $selectedAndForecast.innerHTML = `
-          <section class="selected">
-            <div class="selected-location"></div>
-            <div class="selected-status"></div>
+  Template.prototype.selected = () => {
+    console.log(state);
+    document.querySelector(".selected").innerHTML = `
+            <div class="selected-location">${state.timezone}</div>
+            <div class="selected-status">${
+              state.days[0].weather[0].description[0].toUpperCase() +
+              state.days[0].weather[0].description.slice(1)
+            }</div>
             <div class="selected-summary">
-              <span class="selected-icon"></span>
-              <span class="selected-stats">°</span>
+              <span class="selected-icon">${
+                icons[state.days[0].weather[0].description]
+              }</span>
+              <span class="selected-temp-high">${Math.round(
+                state.days[0].temp.max
+              )}°</span>
+              <span class="selected-temp-low">${Math.round(
+                state.days[0].temp.min
+              )}°</span>
             </div>
-          </section>`;
+          
+          `;
   };
   Template.prototype.forecast = () => {
-    // this.$selectedAndForecast.fistElementChild
-    //       `<section class="forecast">
-    //         <ul class="forecast-list">
-    //           <li class="forecast-item">
-    //             <div class="item-name">${state}</div>
-    //             <span class="item-icon">${state}</span>
-    //             <span class="item-temp-high">${state}°</span>
-    //             <span class="item-temp-low">${state}°</span>
-    //           </li>
-    //         </ul>
-    //       </section>`;
+    let listItems = "";
+
+    state.days.forEach((day) => {
+      listItems += `
+                    <li class="forecast-item">
+                      <div class="item-name">${new Date(day.dt * 1000)
+                        .toString()
+                        .slice(0, 3)
+                        .toUpperCase()}</div>
+                      <span class="item-icon">${
+                        icons[day.weather[0].description]
+                      }</span>
+                      <span class="item-temp-high">${Math.round(
+                        day.temp.max
+                      )}°</span>
+                      <span class="item-temp-low">${Math.round(
+                        day.temp.min
+                      )}°</span>
+                    </li>
+                  `;
+    });
+    document.querySelector(".forecast-list").innerHTML = listItems;
   };
 
   //   <section class="selected-and-forecast">
@@ -76,8 +110,10 @@ const template = new Template();
 // functions
 const render = () => {
   state = JSON.parse(localStorage.getItem("weatherState"));
+  template.init();
   template.current();
   template.selected();
+  template.forecast();
 };
 
 const setState = (current, daily, timezone) => {
@@ -90,7 +126,7 @@ const setState = (current, daily, timezone) => {
         description: current.weather[0].description,
         mainly: current.weather[0].main,
       },
-      days: daily.slice(0, 4),
+      days: daily.slice(0, 5),
       timezone,
     })
   );
@@ -99,9 +135,7 @@ const setState = (current, daily, timezone) => {
 const getData = async () => {
   const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${37.5326}&lon=${127.024612}&exclude=hourly${needForecast}&appid=${API_KEY}&units=${unitSetting}`;
 
-  const response = await fetch(URL).catch((e) => {
-    console.log(new Error(e));
-  });
+  const response = await fetch(URL).catch(console.log);
   const data = await response.json();
   const { current, daily, timezone } = data;
 
@@ -111,6 +145,17 @@ const getData = async () => {
 const initWeather = async () => {
   if (!state) await getData();
   render();
+};
+
+$layoutWeather.onclick = ({ target }) => {
+  if (!target.matches(".location")) return;
+  const $modal = document.querySelector(".selected-forecast");
+  $modal.classList.toggle("weather-active");
+};
+
+$layoutWeather.onclick = ({ target }) => {
+  if (!target.matches(".forecast-item")) return;
+  console.log(target);
 };
 
 export { initWeather };
