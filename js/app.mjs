e70c6f0b384d670eb5dabd091c24bec618c40b29
clowns123 <clonws1232@gmail.comm>
2020-06-09 16:24:40 +0900

    solve: index.html merge

--- js/app.mjs
+++ js/app.mjs
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import { initBackground } from "./src/background.mjs";
-import { initWeather } from "./src/weather.mjs";
-import { renderClock as initClock } from "./src/clock.mjs";
-import { getQuote as initQuote }from "./src/quote.mjs";
-
-window.onload = () => {
-  initClock();
-  initQuote();
-  initBackground();
-  initWeather();
-};
-=======
 import { init as greeting } from './src/greeting.mjs'
 import { init as mainTodo } from './src/mainTodo.mjs'
 import { init as subTodos } from './src/subTodos.mjs'
@@ -24,4 +11,3 @@ window.onload = () => {
   mainTodo()
   subTodos()
 }
->>>>>>> 32392062770d9d1f7c7d2eed4d2bb9694ff5acd4
