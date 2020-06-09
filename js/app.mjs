8efe6f803d04ccaa18d2b3c17d8b8d05c5903d59
soda-cloud <linn45577@gmail.com>
2020-06-09 16:23:37 +0900

    feat: add clock css

--- js/app.mjs
+++ js/app.mjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initBackground } from "./src/background.mjs";
 import { initWeather } from "./src/weather.mjs";
 import { renderClock as initClock } from "./src/clock.mjs";
@@ -10,18 +9,3 @@ window.onload = () => {
   initBackground();
   initWeather();
 };
-=======
-import { init as greeting } from './src/greeting.mjs'
-import { init as mainTodo } from './src/mainTodo.mjs'
-import { init as subTodos } from './src/subTodos.mjs'
-import { initWeather } from './src/weather.mjs'
-import { initBackground } from './src/background.mjs'
-
-window.onload = () => {
-  initWeather()
-  initBackground()
-  greeting()
-  mainTodo()
-  subTodos()
-}
->>>>>>> 32392062770d9d1f7c7d2eed4d2bb9694ff5acd4
